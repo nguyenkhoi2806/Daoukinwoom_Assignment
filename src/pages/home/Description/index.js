fd3a0bcd4bb5b28d94387fba80930d5f655d5519
khoi.le@sunbytes.vn <khoi.le@sunbytes.vn>
2022-01-05 16:48:33 +0700

    add description

---
+++ src/pages/home/Description/index.js
@@ -0,0 +1,16 @@
+import "./style.scss";
+
+import React from "react";
+
+const Description = () => {
+  return (
+    <div className="description">
+      <span>
+        You are now viewing an <strong>IT Software</strong> that has been
+        verified by <strong>over 1 million users.</strong>
+      </span>
+    </div>
+  );
+};
+
+export default Description;
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,11 +3,13 @@ import "./style.scss";
 import React from "react";
 
 import Banner from "./Banner";
+import Description from "./Description";
 
 const Home = () => {
   return (
     <div className="home-page">
       <Banner />
+      <Description />
     </div>
   );
 };
